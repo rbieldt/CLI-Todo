5a4e07470a516687ae70ae3c9e8d30e2d577038d
ruanb <ruan@isaziconsulting.co.za>
2024-11-24 19:39:04 +0200

    Revert "add some structs for config"
    This reverts commit e09aa55abaf4567ac30d759c0a09e08949e86747.

--- go/cli_app/main.go
+++ go/cli_app/main.go
@@ -5,22 +5,6 @@ package main
 
 import "github.com/rbieldt/CLI-Todo/go/cli_app/cmd"
 
-type MySqlOptions struct {
-	User     string
-	Password string
-	Database string
-	Port     int
-}
-
-type GrpcOptions struct {
-	Port int
-}
-
-type Options struct {
-	MySqlOptions *MySqlOptions
-	GrpcOptions  *GrpcOptions
-}
-
 func main() {
 	cmd.Execute()
 }
