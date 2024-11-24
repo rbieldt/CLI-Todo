e09aa55abaf4567ac30d759c0a09e08949e86747
ruanb <ruan@isaziconsulting.co.za>
2024-11-24 19:12:45 +0200

    add some structs for config

--- go/cli_app/main.go
+++ go/cli_app/main.go
@@ -5,6 +5,22 @@ package main
 
 import "github.com/rbieldt/CLI-Todo/go/cli_app/cmd"
 
+type MySqlOptions struct {
+	User     string
+	Password string
+	Database string
+	Port     int
+}
+
+type GrpcOptions struct {
+	Port int
+}
+
+type Options struct {
+	MySqlOptions *MySqlOptions
+	GrpcOptions  *GrpcOptions
+}
+
 func main() {
 	cmd.Execute()
 }
